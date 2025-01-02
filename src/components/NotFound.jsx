26ddaeeb97c90bc72dfa6806a5d9f05edec507fa
samuel2935 <samuelblessed38@gmail.com>
2025-01-02 08:12:49 -0800

    update

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className='mt-72 text-red-300'>
       <h1>Oops! Page not found.</h1>
-      <p><Link to="/">Return home </Link> </p>
+      <h3><Link className='text-emerald-500' to="/">Return home </Link> </h3>
     </div>
   )
 }
