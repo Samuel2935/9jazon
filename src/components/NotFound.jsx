9f3f7eddc150fe67daffd1a2e3a472a71bd217c5
samuel2935 <samuelblessed38@gmail.com>
2025-01-02 08:06:06 -0800

    update

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,11 @@
 
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className='mt-72 text-red-300'>
       <h1>Oops! Page not found.</h1>
+      <p><Link to="/">Return home </Link> </p>
     </div>
   )
 }
